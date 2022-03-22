dcb1d17a3db1a8ed0f35871a24d455c625ef1c57
shinshin86 <beagles1986@gmail.com>
2022-03-23 07:22:05 +0900

    Add test case

--- index.test.js
+++ index.test.js
@@ -5,13 +5,10 @@ const path = require('path');
 const rimraf = require('rimraf');
 
 describe('simple-favicon-generator', () => {
-  test('Minimum test', async () => {
-    const targetImage = './logo/simple-favicon-generator-logo.png';
-    const siteName = 'Minimum test site';
-    const defaultOutputDir = './public';
-
-    const expectWebmanifest = `{"name":"Minimum test site","short_name":"Minimum test site","icons":[{"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},{"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}],"theme_color":"#ffffff","background_color":"#ffffff","display":"standalone"}`;
-    const expectBrowserConfigXML = `<?xml version="1.0" encoding="utf-8"?>
+  const targetImage = './logo/simple-favicon-generator-logo.png';
+  const siteName = 'Minimum test site';
+  const expectWebmanifest = `{"name":"Minimum test site","short_name":"Minimum test site","icons":[{"src":"/android-chrome-192x192.png","sizes":"192x192","type":"image/png"},{"src":"/android-chrome-512x512.png","sizes":"512x512","type":"image/png"}],"theme_color":"#ffffff","background_color":"#ffffff","display":"standalone"}`;
+  const expectBrowserConfigXML = `<?xml version="1.0" encoding="utf-8"?>
 <browserconfig>
     <msapplication>
         <tile>
@@ -24,53 +21,109 @@ describe('simple-favicon-generator', () => {
     </msapplication>
 </browserconfig>`;
 
-    await fsPromises.mkdir(defaultOutputDir);
-    await generateFavicons(targetImage, siteName);
+  describe('Default output dir: public', () => {
+    const outputDir = 'public';
 
-    const webmanifest = await fsPromises.readFile(
-      path.join(defaultOutputDir, 'site.webmanifest'),
-      'utf8'
-    );
-    expect(webmanifest).toBe(expectWebmanifest);
+    test('Minimum test', async () => {
+      await fsPromises.mkdir(outputDir);
+      await generateFavicons(targetImage, siteName);
 
-    const browserConfigXML = await fsPromises.readFile(
-      path.join(defaultOutputDir, 'browserconfig.xml'),
-      'utf8'
-    );
-    expect(browserConfigXML).toBe(expectBrowserConfigXML);
+      const webmanifest = await fsPromises.readFile(
+        path.join(outputDir, 'site.webmanifest'),
+        'utf8'
+      );
+      expect(webmanifest).toBe(expectWebmanifest);
 
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'android-chrome-192x192.png'))
-    ).toBeTruthy();
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'android-chrome-512x512.png'))
-    ).toBeTruthy();
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'apple-touch-icon.png'))
-    ).toBeTruthy();
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'favicon-16x16.png'))
-    ).toBeTruthy();
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'favicon-32x32.png'))
-    ).toBeTruthy();
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'favicon.ico'))
-    ).toBeTruthy();
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'mstile-70x70.png'))
-    ).toBeTruthy();
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'mstile-150x150.png'))
-    ).toBeTruthy();
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'mstile-310x150.png'))
-    ).toBeTruthy();
-    expect(
-      fs.existsSync(path.join(defaultOutputDir, 'mstile-310x310.png'))
-    ).toBeTruthy();
+      const browserConfigXML = await fsPromises.readFile(
+        path.join(outputDir, 'browserconfig.xml'),
+        'utf8'
+      );
+      expect(browserConfigXML).toBe(expectBrowserConfigXML);
 
-    // use sync methods
-    rimraf.sync(defaultOutputDir);
+      expect(
+        fs.existsSync(path.join(outputDir, 'android-chrome-192x192.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'android-chrome-512x512.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'apple-touch-icon.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'favicon-16x16.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'favicon-32x32.png'))
+      ).toBeTruthy();
+      expect(fs.existsSync(path.join(outputDir, 'favicon.ico'))).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'mstile-70x70.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'mstile-150x150.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'mstile-310x150.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'mstile-310x310.png'))
+      ).toBeTruthy();
+
+      // use sync methods
+      rimraf.sync(outputDir);
+    });
+  });
+
+  describe('Specify output dir: testdir', () => {
+    const outputDir = 'testdir';
+
+    test('Minimum test', async () => {
+      await fsPromises.mkdir(outputDir);
+      await generateFavicons(targetImage, siteName, outputDir);
+
+      const webmanifest = await fsPromises.readFile(
+        path.join(outputDir, 'site.webmanifest'),
+        'utf8'
+      );
+      expect(webmanifest).toBe(expectWebmanifest);
+
+      const browserConfigXML = await fsPromises.readFile(
+        path.join(outputDir, 'browserconfig.xml'),
+        'utf8'
+      );
+      expect(browserConfigXML).toBe(expectBrowserConfigXML);
+
+      expect(
+        fs.existsSync(path.join(outputDir, 'android-chrome-192x192.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'android-chrome-512x512.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'apple-touch-icon.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'favicon-16x16.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'favicon-32x32.png'))
+      ).toBeTruthy();
+      expect(fs.existsSync(path.join(outputDir, 'favicon.ico'))).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'mstile-70x70.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'mstile-150x150.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'mstile-310x150.png'))
+      ).toBeTruthy();
+      expect(
+        fs.existsSync(path.join(outputDir, 'mstile-310x310.png'))
+      ).toBeTruthy();
+
+      // use sync methods
+      rimraf.sync(outputDir);
+    });
   });
 });
